d162eb03266828f220e55d7794283f1f83cb37c0
Peng Zhong <peng@nylira.com>
2013-01-06 02:14:50 +0900

    last round

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 $("#play-demo").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+
+ // smooth scrolling to href#
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+    || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+});
